e5ed88001a4c8542345bd7d661eea2c0558d5c5b
Jon LaBelle <jon@tech0.com>
2015-01-14 20:47:43 -0600

    Init commit.

---
+++ README.md
@@ -0,0 +1,25 @@
+# Sublime Text Timestamp
+
+A Sublime Text plug-in to insert a timestamp.
+
+## Install
+
+Download the [zip](https://github.com/jonlabelle/SublimeTimestamp/archive/master.zip) to your Sublime Text 2 or 3 **Packages** directory.
+
+## Usage
+
+Timestamp can be accessed from the Sublime Text command palette...
+
+- `ctrl/cmd + shift + P` : ***Insert Timestamp***
+
+### Example Timestamp
+
+	2015-01-14 20:26:20
+
+### Timestamp Format
+
+	%Y-%m-%d %H:%M:%S
+
+## Author
+
+Jon LaBelle <jon@tech0.com>
---
+++ timestamp.py
@@ -0,0 +1,19 @@
+import datetime
+import sublime_plugin
+import sublime
+
+
+class TimestampCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+
+        timestamp_format = '%Y-%m-%d %H:%M:%S'
+        timestamp_str = datetime.datetime.now().strftime(timestamp_format)
+
+        for region in self.view.sel():
+            if region.empty():
+                self.view.insert(edit, region.a, timestamp_str)
+            else:
+                self.view.replace(edit, region, timestamp_str)
+
+        sublime.set_timeout(lambda: sublime.status_message('Timestamp inserted.'), 0)
